d3edb1623cd0686ad4944332656b318308d28587
Eojinn <aj41212@gmail.com>
2026-03-08 23:25:09 +0900

    MyRunner update

--- src/main/java/com/basic/myspringboot/runner/MyRunner.java
+++ src/main/java/com/basic/myspringboot/runner/MyRunner.java
@@ -1,6 +1,7 @@
 package com.basic.myspringboot.runner;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.core.env.Environment;
@@ -9,10 +10,16 @@ import org.springframework.stereotype.Component;
 @Component
 public class MyRunner implements ApplicationRunner {
 
+    @Value("${myboot.name}")
+    private String name;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
         System.out.println("VM 아규먼트 foo : " + args.containsOption("foo"));
         System.out.println("Program 아규먼트 bar : " + args.containsOption("bar"));
+
+        //Argument 목록 출력하기 (람다식)
+        args.getOptionNames()//Set<String>
+                .forEach(name -> System.out.println(name));
     }
 }
-
