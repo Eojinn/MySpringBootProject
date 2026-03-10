b3d79e818f3cf22c55f7284b4d6c98ebeeca4c10
Eojinn <aj41212@gmail.com>
2026-03-10 11:10:15 +0900

    CustomerTest update

--- src/main/java/com/basic/myspringboot/repository/CustomerRepository.java
+++ src/main/java/com/basic/myspringboot/repository/CustomerRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
+
 public interface CustomerRepository extends JpaRepository<Customer,Long> {
     //finder(query) method
     //1. customerId(고객번호, Unique이므로 중복허용않함)로 조회하는 finder 메서드
