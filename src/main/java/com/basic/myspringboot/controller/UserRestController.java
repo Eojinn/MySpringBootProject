5e83e8de28472c1828a13615c43b591afe9cd35b
Eojinn <aj41212@gmail.com>
2026-03-11 11:22:50 +0900

    ãDelete

--- src/main/java/com/basic/myspringboot/controller/UserRestController.java
+++ src/main/java/com/basic/myspringboot/controller/UserRestController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -40,18 +41,27 @@ public class UserRestController {
     //Id로 User 조회
     @GetMapping("/{id}")
     public User getUserById(@PathVariable Long id) {
-        User existUser = userRepository.findById(id)//Optional<User>
-                .orElseThrow(()->new BusinessException("User Not Found", HttpStatus.NOT_FOUND));//User
+        User existUser = userRepository.findById(id) //Optional<User>
+                .orElseThrow(() -> new BusinessException("User Not Found", HttpStatus.NOT_FOUND));//User
         return existUser;
     }
 
-    //Email 조회하고 수정하기
+    //Email 조회하고 User 수정하기
     @PatchMapping("/{email}/")
-    public User updateUser(@PathVariable String email, @RequestBody User userDetail) {
+    public User updateUser(@PathVariable String email, @RequestBody User userDetail){
         User existUser = userRepository.findByEmail(email)
-                .orElseThrow(()->new BusinessException("User Not Found", HttpStatus.NOT_FOUND));
+                .orElseThrow(() -> new BusinessException("User Not Found", HttpStatus.NOT_FOUND));
         existUser.setName(userDetail.getName());
         return userRepository.save(existUser);
+    }
 
+    //User 삭제하기
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new BusinessException("User Not Found", HttpStatus.NOT_FOUND));
+        userRepository.delete(user);
+        //return ResponseEntity.ok(user);
+        return ResponseEntity.ok().build();
     }
 }
