6f43259a65a9aef999fe9c2d7ae52e21bb909f34
Eojinn <aj41212@gmail.com>
2026-03-11 09:42:13 +0900

    ãbizexception update

--- src/main/java/com/basic/myspringboot/exception/BusinessException.java
+++ src/main/java/com/basic/myspringboot/exception/BusinessException.java
@@ -1,7 +1,11 @@
 package com.basic.myspringboot.exception;
 
+import com.basic.myspringboot.entity.User;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 @Getter
 public class BusinessException extends RuntimeException {
     private static final long serialVersionUID = 1L;
@@ -17,4 +21,6 @@ public class BusinessException extends RuntimeException {
         this.message = message;
         this.httpStatus = httpStatus;
     }
+
+
 }
